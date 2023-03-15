a43c5f46be53df75d0f1095a7e146a212077abf1
TringApps-Nithish <123442599+TringApps-Nithish@users.noreply.github.com>
2023-03-15 11:52:41 +0530

    Update Main.java

--- src/main/java/mainclass/Main.java
+++ src/main/java/mainclass/Main.java
@@ -11,12 +11,12 @@ public class Main {
     private static Logger l = Logger.getLogger("Switchch");
 
     public static void main(String[] args) {
-        int Choice = 0;
-        while (Choice != 4) {
+        int choice = 0;
+        while (choice != 4) {
             l.info("t...... Shapes Availabel ......\n 1.Triangle \n 2.Rectangle \n 3.Circle \n 4.Exit ");
             l.info("\n\n Enter Your Choice : ");
-            Choice = sc.nextInt();
-            new Switching(Choice);
+            choice = sc.nextInt();
+            new Switching(choice);
         }
     }
 }
